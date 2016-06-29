1165b176fc77e70fac8e0dbb24469471378deb3e
Guillaume TALBOT <talbotgui@gmail.com>
2016-06-29 08:39:35 +0200

    Formattage de source

--- mariageMetier/src/main/java/com/github/talbotgui/mariage/metier/dao/InviteRepository.java
+++ mariageMetier/src/main/java/com/github/talbotgui/mariage/metier/dao/InviteRepository.java
@@ -50,12 +50,14 @@ public interface InviteRepository extends PagingAndSortingRepository<Invite, Lon
 
 	@Query("select distinct i" //
 			+ " from Invite i left join fetch i.presencesEtape "//
-			+ " where i.mariage.id=:idMariage order by i.groupe, i.foyer, i.nom, i.prenom")
+			+ " where i.mariage.id=:idMariage"//
+			+ " order by i.groupe, i.foyer, i.nom, i.prenom")
 	Collection<Invite> listeInvitesParIdMariage(@Param("idMariage") Long idMariage);
 
 	@Query("select i "//
 			+ " from Invite i"//
-			+ " where i.mariage.id=:idMariage order by i.groupe, i.nom")
+			+ " where i.mariage.id=:idMariage"//
+			+ " order by i.groupe, i.nom")
 	Page<Invite> listeInvitesParIdMariage(@Param("idMariage") Long idMariage, Pageable page);
 
 	@Transactional(readOnly = false)
