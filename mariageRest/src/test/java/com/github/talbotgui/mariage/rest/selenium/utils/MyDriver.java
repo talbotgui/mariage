0278defcb31005e97f7ebfb2c2a3dc872b653c24
Guillaume TALBOT <talbotgui@gmail.com>
2016-07-25 11:59:19 +0200

    Refactoring pour faire des ré-essais plus propres

--- mariageRest/src/test/java/com/github/talbotgui/mariage/rest/selenium/utils/MyDriver.java
+++ mariageRest/src/test/java/com/github/talbotgui/mariage/rest/selenium/utils/MyDriver.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertNotNull;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
@@ -21,6 +23,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
+import com.github.rholder.retry.RetryException;
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.RetryerBuilder;
+import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.WaitStrategies;
+
 /**
  * Driver personnalisé pour simplifier l'écriture des tests Selenium
  */
@@ -32,6 +40,9 @@ public class MyDriver {
 
 	/** Le driver réel. */
 	private final WebDriver driver;
+
+	/** Utilitaire pour traiter les ré-essais. */
+	private final Retryer<Void> retryer;
 
 	/**
 	 * Constructeur avec initialisation
@@ -48,44 +59,48 @@ public class MyDriver {
 
 		// Initialisation du timeout par défaut
 		driver.manage().timeouts().implicitlyWait(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+
+		// Définition de la stratégie de ré-essai
+		final int nbEssais = 6;
+		final int tempsEntreDeuxEssaisEnMs = 500;
+		this.retryer = RetryerBuilder.<Void>newBuilder()//
+				.retryIfExceptionOfType(AssertionError.class).retryIfRuntimeException()//
+				.withStopStrategy(StopStrategies.stopAfterAttempt(nbEssais))//
+				.withWaitStrategy(WaitStrategies.fixedWait(tempsEntreDeuxEssaisEnMs, TimeUnit.MILLISECONDS))//
+				.build();
 	}
 
 	public void assertChecked(final By by, final boolean checked) {
-		try {
-			assertEquals(checked, driver.findElement(by).isSelected());
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			assertEquals(checked, driver.findElement(by).isSelected());
-		}
+		final Callable<Void> assertion = () -> {
+			assertEquals(checked, this.driver.findElement(by).isSelected());
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void assertCookieNotPresentOrValid(final String cookieName) {
-		try {
-			final Cookie cookie = driver.manage().getCookieNamed(cookieName);
+		final Callable<Void> assertion = () -> {
+			final Cookie cookie = this.driver.manage().getCookieNamed(cookieName);
 			Assert.assertTrue(cookie == null || cookie.getExpiry().before(new Date()));
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			final Cookie cookie = driver.manage().getCookieNamed(cookieName);
-			Assert.assertTrue(cookie == null || cookie.getExpiry().before(new Date()));
-		}
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void assertCookiePresentAndValid(final String cookieName) {
-		try {
-			final Cookie cookie = driver.manage().getCookieNamed(cookieName);
+		final Callable<Void> assertion = () -> {
+			final Cookie cookie = this.driver.manage().getCookieNamed(cookieName);
 			Assert.assertTrue(cookie != null && cookie.getExpiry().after(new Date()));
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			final Cookie cookie = driver.manage().getCookieNamed(cookieName);
-			Assert.assertTrue(cookie != null && cookie.getExpiry().after(new Date()));
-		}
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void assertElementNotPresent(final By by) {
 		try {
-			WebElement e = driver.findElement(by);
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			e = driver.findElement(by);
+			WebElement e = this.driver.findElement(by);
+			this.sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
+			e = this.driver.findElement(by);
 			Assert.fail("L'élement '" + by.toString() + "' est présent : " + e.toString());
 		} catch (final NoSuchElementException e) {
 			// OK
@@ -93,44 +108,40 @@ public class MyDriver {
 	}
 
 	public void assertElementPresent(final By by) {
-		try {
-			assertNotNull(driver.findElement(by));
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			assertNotNull(driver.findElement(by));
-		}
+		final Callable<Void> assertion = () -> {
+			assertNotNull(MyDriver.this.driver.findElement(by));
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void assertPageTitle(final String title) {
-		try {
-			assertEquals(title, driver.getTitle());
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			assertEquals(title, driver.getTitle());
-		}
+		final Callable<Void> assertion = () -> {
+			assertEquals(title, this.driver.getTitle());
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void assertTextEquals(final By by, final String text) {
-		try {
-			assertEquals(text, driver.findElement(by).getText());
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			assertEquals(text, driver.findElement(by).getText());
-		}
+		final Callable<Void> assertion = () -> {
+			assertEquals(text, this.driver.findElement(by).getText());
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void assertValueEquals(final By by, final String value) {
-		try {
-			assertEquals(value, driver.findElement(by).getAttribute("value"));
-		} catch (final AssertionError e) {
-			sleepSilencieux(NB_MS_ATTENTE_SI_ASSERTION_ERROR);
-			assertEquals(value, driver.findElement(by).getAttribute("value"));
-		}
+		final Callable<Void> assertion = () -> {
+			assertEquals(value, this.driver.findElement(by).getAttribute("value"));
+			return null;
+		};
+		this.retry(assertion);
 	}
 
 	public void click(final By by, final int timeToWait) {
-		driver.findElement(by).click();
-		sleepSilencieux(timeToWait);
+		this.driver.findElement(by).click();
+		this.sleepSilencieux(timeToWait);
 	}
 
 	public void count(final By by, final int count) {
@@ -139,7 +150,7 @@ public class MyDriver {
 
 	public void createSnapshot(final String filename) {
 		try {
-			final File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			final File scrFile = ((TakesScreenshot) this.driver).getScreenshotAs(OutputType.FILE);
 			final File file = new File(filename);
 			FileUtils.copyFile(scrFile, file);
 		} catch (final WebDriverException | IOException e) {
@@ -152,8 +163,8 @@ public class MyDriver {
 	}
 
 	public void executeScript(final String script, final int timeToWait) {
-		((JavascriptExecutor) driver).executeScript(script);
-		sleepSilencieux(timeToWait);
+		((JavascriptExecutor) this.driver).executeScript(script);
+		this.sleepSilencieux(timeToWait);
 	}
 
 	public void get(final String baseUrl) {
@@ -161,16 +172,34 @@ public class MyDriver {
 	}
 
 	public WebDriver getRealDriver() {
-		return driver;
+		return this.driver;
 	}
 
 	public void quit() {
 		this.driver.quit();
 	}
 
+	private void retry(final Callable<Void> assertion) {
+		try {
+			this.retryer.call(assertion);
+		} catch (final RetryException e) {
+			if (e.getCause() != null && e.getCause() instanceof RuntimeException) {
+				throw (RuntimeException) e.getCause();
+			} else if (e.getCause() != null && e.getCause() instanceof Error) {
+				throw (Error) e.getCause();
+			} else if (e.getCause() != null) {
+				throw new RuntimeException(e.getCause());
+			} else {
+				throw new RuntimeException(e);
+			}
+		} catch (final ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public void select(final By by, final String visibleText, final int timeToWait) {
 		new Select(this.driver.findElement(by)).selectByVisibleText(visibleText);
-		sleepSilencieux(timeToWait);
+		this.sleepSilencieux(timeToWait);
 	}
 
 	private void sleepSilencieux(final int timeToWait) {
@@ -184,10 +213,10 @@ public class MyDriver {
 	public void type(final By by, final String value, final int timeToWait) {
 		this.assertElementPresent(by);
 
-		final WebElement e = driver.findElement(by);
+		final WebElement e = this.driver.findElement(by);
 		e.clear();
 		e.sendKeys(value);
-		sleepSilencieux(timeToWait);
+		this.sleepSilencieux(timeToWait);
 	}
 
 }
