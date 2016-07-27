06a7ef0b2a4802f79c6c5b939d8cbfb413029531
Guillaume TALBOT <talbotgui@gmail.com>
2016-07-27 15:12:59 +0200

    Suppression de la glue dans la configuration de Cucumber

--- mariageRest/src/test/java/com/github/talbotgui/mariage/rest/cucumber/TestCucumber.java
+++ mariageRest/src/test/java/com/github/talbotgui/mariage/rest/cucumber/TestCucumber.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = true, glue = { "com.github.talbotgui.mariage.rest.cucumber", "cucumber.api.spring" })
+@CucumberOptions(strict = true, monochrome = true)
 public class TestCucumber {
 
 }
