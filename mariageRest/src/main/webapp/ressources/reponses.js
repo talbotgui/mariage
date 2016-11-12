1cb2d579286a150589733e69bcd843808987619b
Guillaume TALBOT <talbotgui@gmail.com>
2016-11-12 11:06:42 +0100

    amelioration de l'ergonomie

--- mariageRest/src/main/webapp/ressources/reponses.js
+++ mariageRest/src/main/webapp/ressources/reponses.js
@@ -1,5 +1,4 @@
 var modifiePresence = function(radio) {
-console.debug("modifiePresence");
 	// get données
 	var rowIndex = radio.name.substring("present".length);
 	var rowData = $("#reponses").jqxGrid('getrowdata', rowIndex);
@@ -16,8 +15,24 @@ console.debug("modifiePresence");
 	modifieReponse(e);
 }
 
+var modifieConfirme = function(checkbox) {
+	// get données
+	var rowIndex = checkbox.name.substring("confirme".length);
+	var rowData = $("#reponses").jqxGrid('getrowdata', rowIndex);
+
+	// creation données
+	e = {args: {
+		row: rowData,
+		datafield: "confirmee",
+		value: checkbox.value,
+		oldvalue: ""
+	}};
+
+	// modification données
+	modifieReponse(e);
+}
+
 var modifieReponse = function(e) {
-console.debug("modifieReponse");
 	majAttribute(REST_PREFIX + "/mariage/" + idMariage + "/presence", e, chargePresences);
 }
 
@@ -26,7 +41,7 @@ var donneesDejaChargees = false;
 var chargePresences = function() {
 	
 	if (donneesDejaChargees) {
-		$("#reponses").jqxGrid('updatebounddata', 'cells');
+		//$("#reponses").jqxGrid('updatebounddata', 'cells');
 	
 	} else {
 		
@@ -35,6 +50,11 @@ var chargePresences = function() {
 			if (value === true) { checkedOui = "checked='checked'";
 			} else if (value === false) { checkedNon = "checked='checked'"; }
 			return "<input name='" + name +"' value='true' type='radio' onchange='modifiePresence(this)' " + checkedOui + ">oui</input>&nbsp;<input name='" + name +"' value='false' type='radio' onchange='modifiePresence(this)' " + checkedNon + ">non</input>"; 
+		};
+		var rendererColonneConfirme = function (rowIndex, columnfield, value, defaulthtml, columnproperties, objet) {
+			var name = "confirme" + rowIndex; var checked = "";
+			if (value === true) { checked = "checked='checked'"; }
+			return "<div style='text-align:center'><input name='" + name +"' type='checkbox' onchange='modifieConfirme(this)' " + checked + "></input></div>"; 
 		};
 
 		// Initialisation des fields et column fixes
@@ -46,8 +66,8 @@ var chargePresences = function() {
 						{ text: 'Etape', datafield: 'nomEtape', editable: false, width: '10%' },
 						{ text: 'Mise à jour', datafield: 'dateMaj', editable: false, width: '12%', cellsformat: 'dd/MM/yy HH:mm' },
 						{ text: 'Présence', datafield: 'present', editable: false, width: '10%', cellsrenderer: rendererColonnePresence },
-						{ text: 'Confirmation', datafield: 'confirmee', editable: true, width: '10%', columntype: 'checkbox' },
-						{ text: 'Commentaire', datafield: 'commentaire', editable: true, width: '40%' }
+						{ text: 'Confirmation', datafield: 'confirmee', editable: false, width: '10%', cellsrenderer: rendererColonneConfirme },
+						{ text: 'Commentaire', datafield: 'commentaire', editable: true, width: '35%' }
 		];
 
 		var dataAdapter = new $.jqx.dataAdapter({
@@ -63,11 +83,11 @@ var chargePresences = function() {
 			pageable: true, pagesizeoptions: ['20', '50', '100'],
 			editable: true, sortable: true, filterable: true, autoheight: true, altrows: true,
 			width: 950,
-			ready: function() {
-				afficheContent();
-			}
+			ready:  function() { afficheContent(); }
 		});
 		$("#reponses").on('cellendedit', modifieReponse);
+		$("#reponses").on("pagechanged", function (event) { $("#reponses").jqxGrid('updatebounddata'); });
+		$("#reponses").on("sort", function (event) { $("#reponses").jqxGrid('updatebounddata'); });
 		donneesDejaChargees = true;
 	}
 };
