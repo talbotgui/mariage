c248c91fb6f47219cc1208f2764b244356c5c7e3
Guillaume TALBOT <talbotgui@gmail.com>
2016-11-12 08:58:30 +0100

    Refonte de la gestion du menu changeant en fonction du role pour ne pas avoir de redirection (posant pb https/http)

--- mariageRest/src/main/java/com/github/talbotgui/mariage/rest/controleur/MenuControler.java
+++ mariageRest/src/main/java/com/github/talbotgui/mariage/rest/controleur/MenuControler.java
@@ -2,17 +2,12 @@ package com.github.talbotgui.mariage.rest.controleur;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.Scanner;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,19 +16,16 @@ import com.github.talbotgui.mariage.rest.security.SecurityFilter;
 @RestController
 public class MenuControler {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MenuControler.class);
-
 	@RequestMapping(value = "/part_menu.html", method = GET, produces = "text/html;charset=UTF-8")
 	public String getMenu(final HttpServletRequest request, final HttpServletResponse response) {
 		final String roleUtilisateur = (String) request.getSession().getAttribute(SecurityFilter.SESSION_KEY_USER_ROLE);
 		final String nomPageMenu = "part_menu_" + roleUtilisateur + ".html";
 
-		try {
-			final Path path = Paths.get(this.getClass().getClassLoader().getResource(nomPageMenu).toURI());
-			return new String(Files.readAllBytes(path), "utf8");
-		} catch (final IOException | URISyntaxException e) {
-			LOG.error("Erreur de chargement du menu", e);
-			return "";
+		// L'accès par Files/Paths ne fonctionne pas quand le fichier est dans
+		// un WAR
+		final InputStream in = this.getClass().getClassLoader().getResourceAsStream(nomPageMenu);
+		try (Scanner scanner = new Scanner(in, "UTF-8")) {
+			return scanner.useDelimiter("\\A").next();
 		}
 	}
 }
